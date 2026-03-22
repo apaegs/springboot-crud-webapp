6a44dca9787e8f4de1c0786d63d1099525f97017
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-03-22 15:14:45 +0100

    Refactor MovieRepository inheritance and cleanup controller
    - Update `MovieRepository` to extend `ListCrudRepository` and `PagingAndSortingRepository` instead of `JpaRepository`.
    - Remove unused `isBlank` helper method from `MovieController`.

--- src/main/java/org/noob/springbootcrudwebapp/repository/MovieRepository.java
+++ src/main/java/org/noob/springbootcrudwebapp/repository/MovieRepository.java
@@ -3,8 +3,9 @@ package org.noob.springbootcrudwebapp.repository;
 import org.noob.springbootcrudwebapp.entity.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface MovieRepository extends ListCrudRepository<Movie, Long>, PagingAndSortingRepository<Movie, Long> {
 
     @Query("""
             SELECT m FROM Movie m WHERE
