2e4ca21ea2324f52451e2606ef7a896bcd152a26
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-03-22 13:17:28 +0100

    Add MovieFilterDto for filtering movie results
    - Create `MovieFilterDto` class to handle search criteria.
    - Define fields for filtering by title, director, release date range (`from`, `to`), and duration limits (`minDuration`, `maxDuration`).

---
+++ src/main/java/org/noob/springbootcrudwebapp/dto/MovieFilterDto.java
@@ -0,0 +1,63 @@
+package org.noob.springbootcrudwebapp.dto;
+
+import java.time.LocalDate;
+
+public class MovieFilterDto {
+
+    private String title;
+    private String director;
+    private LocalDate from;
+    private LocalDate to;
+    private Integer minDuration;
+    private Integer maxDuration;
+
+    public MovieFilterDto() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public LocalDate getFrom() {
+        return from;
+    }
+
+    public void setFrom(LocalDate from) {
+        this.from = from;
+    }
+
+    public LocalDate getTo() {
+        return to;
+    }
+
+    public void setTo(LocalDate to) {
+        this.to = to;
+    }
+
+    public Integer getMinDuration() {
+        return minDuration;
+    }
+
+    public void setMinDuration(Integer minDuration) {
+        this.minDuration = minDuration;
+    }
+
+    public Integer getMaxDuration() {
+        return maxDuration;
+    }
+
+    public void setMaxDuration(Integer maxDuration) {
+        this.maxDuration = maxDuration;
+    }
+}
